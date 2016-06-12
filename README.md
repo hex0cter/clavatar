5585e1c2045507a37174e79df968ee3ada89c0ef
Daniel Han <hex0cter@gmail.com>
2016-06-12 23:15:31 +0200

    code refactoring

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 [![Gem Version](https://badge.fury.io/rb/clavatar.svg)](https://badge.fury.io/rb/clavatar)
 [![Build Status](https://travis-ci.org/hex0cter/clavatar.svg?branch=master)](https://travis-ci.org/hex0cter/clavatar)
-[![Coverage Status](https://coveralls.io/repos/github/hex0cter/clavatar/badge.svg?branch=master)](https://coveralls.io/github/hex0cter/clavatar?branch=master)
 
 Clavatar is a ruby gem that converts a hash into a Ruby object.
 
--- lib/clavatar.rb
+++ lib/clavatar.rb
@@ -14,16 +14,15 @@ module Clavatar
     args = {}
     construct_params = example.method(:initialize).parameters
     construct_params.each do |param|
-      arg_required = param.first
-      arg_name = param.last
+      arg_required, arg_name = param
 
-      if arg_required == :keyreq
-        raise "parameter #{arg_name} is mandatory but missing in #{hash}" unless hash.key? arg_name
-      elsif arg_required == :keyrest
-        args.merge! hash
-        break
+      case arg_required
+        when :keyreq
+          raise "parameter #{arg_name} is mandatory but missing in #{hash}" unless hash.key? arg_name
+        when :keyrest
+          args.merge! hash
+          break
       end
-
       args[arg_name] = hash[arg_name]
     end
 
@@ -31,6 +30,7 @@ module Clavatar
     accessible_attr.each do |attr|
       instance.send("#{attr}=", hash[attr])
     end
-    return instance
+
+    instance
   end
 end
